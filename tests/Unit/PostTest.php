53c129f3479cc44263349ae17423ae424f8f877d
Sandeshwar Sharma <dev.sandeshwar@outlook.com>
2018-09-18 12:47:47 +0530

    add post createdAt() test

---
+++ tests/Unit/PostTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Post;
+
+class PostTest extends TestCase
+{
+    /**
+     * Should have createdAt() which returns formatted created at date
+     * 
+     * @group post-created-at
+     * @return void
+     */
+    public function testCanGetCreatedAtFormattedDate() 
+    {
+        // create a post
+        $post = Post::create([
+            'title' => 'Swalla la le everyday',
+            'body'  => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dolorum ullam facere repellendus earum fuga reiciendis'
+        ]);
+
+        // call the method
+        $formattedDate = $post->createdAt();
+
+        // match the dates
+        $this->assertCheck($post->created_at->toFormattedDateString(), $post->createdAt());
+    }
+}
