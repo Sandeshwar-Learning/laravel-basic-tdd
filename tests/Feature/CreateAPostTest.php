d995298772c7af7f5d51779d6b8cafd27e071541
Sandeshwar Sharma <dev.sandeshwar@outlook.com>
2018-09-20 12:40:50 +0530

    add test for creating a post

---
+++ tests/Feature/CreateAPostTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Post;
+
+class CreateAPostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test to check if a post can be created
+     *
+     * @group create-post
+     * @return void
+     */
+    public function testCanCreateAPost()
+    {
+        $post = new Post();
+        $post->title = 'My Post';
+        $post->body = 'My Post Body';
+
+        // make post request
+        $this->post('/posts', [
+            'title' => $post->title,
+            'body' => $post->body
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => $post->title,
+            'body' => $post->body
+        ]);
+
+        $savedPost = Post::where(['title' => $post->title]);
+
+        $this->assertEquals($post->title, $savedPost->title);
+    }
+}
